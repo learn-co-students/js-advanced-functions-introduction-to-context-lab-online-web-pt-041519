99e5f563bc1f02e74cc43a9d60d8d39ad193847f
Renee Nordholm <reneenordholm@gmail.com>
2019-11-29 20:17:10 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+function createEmployeeRecord(array) {  
+    const employeeRecord = Object.assign({}, {
+        firstName: array[0],
+        familyName: array[1], 
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    })
+    return employeeRecord
+}
+
+const createEmployeeRecords = function(array) {
+    const employeeRecord = array.map(a => createEmployeeRecord(a))
+    return employeeRecord
+}
+
+function createTimeInEvent() {
+    
+}
